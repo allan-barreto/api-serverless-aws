7a2fb324e66ed1b17c5445e06bdc14c041ee7603
Allan Barreto <allanbarreto89@gmail.com>
2023-06-26 17:35:45 -0300

    terraform deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,8 +16,6 @@ terraform {
 
 provider "aws" {
   region     = var.aws_region
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 
 resource "random_pet" "website" {
